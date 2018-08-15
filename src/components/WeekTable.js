c338238f88c8d98ec4bb2f41c4cdb2c3c19d00e8
Soulfilip <filip9322@gmail.com>
2018-08-14 23:13:40 -0500

    Modify html- Visualice as Table and rows

--- src/components/WeekTable.js
+++ src/components/WeekTable.js
@@ -14,37 +14,54 @@ import './WeekTable.css';
 class WeekDaysNav extends React.Component {
 	render() {
 		return(
-		<ul>
+		<table>
+			<tr>
+				<th>Weekday</th>
 			{
 				WEEKDAYS.map((weekday) =>{
-					console.log(this);
 					let isWeekDay = weekday.weekday;
 					return(
-						<li key={weekday.name}>
+						<th key={weekday.name}>
 						{ isWeekDay ? (
 								<span className="color-red">{weekday.kanji}</span>
 							):(
 								<span className="color-blue">{weekday.kanji}</span>
 							)
 						}
-						</li>
+						</th>
 						);
 				})
 			}
-		</ul>
+			</tr>
+			<tr>
+				<td>Tarea 1</td>
+				<td>O</td>
+				<td>X</td>
+			</tr>
+			<tr>
+				<td>Tarea 2</td>
+				<td>O</td>
+				<td>X</td>
+			</tr>
+			<tr>
+				<td>Tarea 3</td>
+				<td>O</td>
+				<td>X</td>
+			</tr>
+		</table>
 		);
 	}
 
 }
 
 const WEEKDAYS = [
+  {name: 'Domingo'    ,kanji: '日', weekday: false},
   {name: 'Lunes'      ,kanji: '月', weekday: true },
   {name: 'Martes'     ,kanji: '火', weekday: true },
   {name: 'Miercoles'  ,kanji: '水', weekday: true },
   {name: 'Jueves'     ,kanji: '木', weekday: true },
   {name: 'Viernes'    ,kanji: '金', weekday: true },
   {name: 'Sabado'     ,kanji: '土', weekday: false},
-  {name: 'Domingo'    ,kanji: '日', weekday: false},
 ];
 
 export default WeekDaysNav;
