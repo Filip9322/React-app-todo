bc33d20d901e422917523fa707b88f4e1fcc542a
Soulfilip <filip9322@gmail.com>
2018-09-03 22:21:13 -0500

    setting up module of state info

--- src/components/DetailedTask.js
+++ src/components/DetailedTask.js
@@ -12,11 +12,44 @@ class DetailsFromTask extends Component {
 				<div className = "detailed-task">
 					<label>{ this.props.detailedTask }</label>
 				</div>
-				
+				<InfoStates states = {STATES} />
 			</fieldset>
 		);
 	}
 }
-
+class InfoStates extends Component {
+	render() {
+		return(
+			<div className = "info-state">
+				{
+					this.props.states.map((state) =>{
+						return(
+							<div className = "state-item">
+								<p>{ state }</p>
+							</div>
+						);
+					})
+				}
+			</div>
+		);
+	}
+}
+const STATES = [
+	"StateToDo": {
+      "_id"     : 1,
+      "value"  : "",
+      "description": "todo"
+    },
+    "StateDone" : {
+      "_id"   : 2,
+      "value": "O",
+      "description": "done"
+    },
+    "StateUndone" : {
+      "_id"   : 3,
+      "value": "X",
+      "description": "undone"
+    }
+];
 
 export default DetailsFromTask;
